55128172412363615d523c9831fcb3388ed174c3
TimSchonning <116971347+TimSchonning@users.noreply.github.com>
2026-03-25 18:54:40 +0100

    Arduino setup() update
    Added necessary power optimizations

--- node/src/main.cpp
+++ node/src/main.cpp
@@ -1,5 +1,8 @@
 #include <Seeed_HM330X.h>
 #include <HM330X.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
+#include <esp_bt.h>
 
 /* Sound sensor */
 
@@ -11,6 +14,12 @@ pm_res ps_results;
 
 
 void setup() {
+    /* Remember to solder off the LED */
+    setCpuFrequencyMhz(10);     /* Might be too low */
+    WiFi.mode(WIFI_OFF);
+    btStop();
+    esp_wifi_stop();
+    esp_bt_controller_disable();
 
 }
 
