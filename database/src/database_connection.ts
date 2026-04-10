613f16c845a8905c10c1b94dd99428ebcbfedc97
Filip Shellbranch <filip.hellgren04@gmail.com>
2026-04-10 10:52:33 +0200

    feat: added a temporary auth solution using anonymous sign in

--- database/src/database_connection.ts
+++ database/src/database_connection.ts
@@ -1,15 +1,27 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { Firestore, getFirestore } from "firebase/firestore";
+import { Auth, getAuth, signInAnonymously } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    // Vite uses import.meta.env instead of process.env
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = getApps().length == 0 ? initializeApp(firebaseConfig) : getApp();
 
 export const db: Firestore = getFirestore(app);
+export const auth: Auth = getAuth(app);
+
+export async function verifySignedIn() {
+    if (auth.currentUser == null) {
+        await signInAnonymously(auth);
+        console.log("Anonymous sign in complete!");
+    }
+}
+
+verifySignedIn();
