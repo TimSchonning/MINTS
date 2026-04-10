91761a4d3b8cc7eb9e879359b0fa73d225224f4e
Filip Shellbranch <filip.hellgren04@gmail.com>
2026-04-10 12:05:42 +0200

    feat: added a temporary test button to add a station to firebase

---
+++ frontend/src/myapp/src/lib/TestButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { create_station } from '@my-app/database';
+
+	let isAdded: boolean = $state(false);
+	async function testStoringStation() {
+		console.log('Attempting to create a station');
+
+		await create_station((1).toString(), { lat: 2, lng: 4 });
+		isAdded = true;
+	}
+</script>
+
+{#if isAdded}
+	<h1>A station has been added to the database!</h1>
+{:else}
+	<button onclick={testStoringStation} style="cursor: pointer;">
+		Click me to test adding a station to firebase
+	</button>
+{/if}
