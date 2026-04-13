17d71d3b4f9fac26146cec0184131fa341688cfc
Filip Shellbranch <filip.hellgren04@gmail.com>
2026-04-13 10:53:37 +0200

    fix: fixed imports from heatmap after move

--- frontend/src/myapp/src/lib/Map.svelte
+++ frontend/src/myapp/src/lib/Map.svelte
@@ -1,48 +1,47 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import mapboxgl from 'mapbox-gl';
-  import 'mapbox-gl/dist/mapbox-gl.css';
-  import { toGeoJSON, testData } from '../../../../../backend/src/heatmap.ts';
+	import { onMount } from 'svelte';
+	import mapboxgl from 'mapbox-gl';
+	import 'mapbox-gl/dist/mapbox-gl.css';
+	import { toGeoJSON, testData } from '../heatmap';
 
-  let map : mapboxgl.Map;
+	let map: mapboxgl.Map;
 
-  onMount(() => {
-      mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+	onMount(() => {
+		mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
-      map = new mapboxgl.Map({
-          container: 'map',
-          style: 'mapbox://styles/mapbox/standard',
-          center: [17.635488, 59.843905],
-          zoom: 12
-      });
+		map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/standard',
+			center: [17.635488, 59.843905],
+			zoom: 12
+		});
 
-      map.addControl(new mapboxgl.NavigationControl());
-      
-      map.on('load', () => {
-          map.addSource('heat', {
-            type: 'geojson',
-            data: toGeoJSON([])
-          });
+		map.addControl(new mapboxgl.NavigationControl());
 
-          map.addLayer({
-              id: 'heatmap',
-              type: 'heatmap',
-              source: 'heat',
-              maxzoom: 20,
-              paint: {
-                'heatmap-weight': ['get', 'intensity'],
-                'heatmap-radius': 20
-              }
-              });
-          testData.subscribe(data => {
-            const source = map.getSource('heat') as mapboxgl.GeoJSONSource;
-            if (source) {
-              source.setData(toGeoJSON(data));
-            }
-          })
-  });
-});
+		map.on('load', () => {
+			map.addSource('heat', {
+				type: 'geojson',
+				data: toGeoJSON([])
+			});
 
+			map.addLayer({
+				id: 'heatmap',
+				type: 'heatmap',
+				source: 'heat',
+				maxzoom: 20,
+				paint: {
+					'heatmap-weight': ['get', 'intensity'],
+					'heatmap-radius': 20
+				}
+			});
+			testData.subscribe((data) => {
+				const source = map.getSource('heat') as mapboxgl.GeoJSONSource;
+				if (source) {
+					source.setData(toGeoJSON(data));
+				}
+			});
+		});
+	});
 </script>
 
 <div id="map" style="width: 100vw; height: 100vh;"></div>
--- frontend/src/myapp/src/lib/Menu.svelte
+++ frontend/src/myapp/src/lib/Menu.svelte
@@ -1,145 +1,142 @@
 <script lang="ts">
-import { addLayer, removeLayer } from '../../../../../backend/src/heatmap.ts';
+	import { addLayer, removeLayer } from '../heatmap';
 
-let menuItems = [
-        {
-            'name': 'Noise',
-            'displayText': "Noise",
-            'checked': false
-        },
-        {
-            'name': 'PM2.5',
-            'displayText': "PM2.5",
-            'checked': false
-        },
-        {
-            'name': 'PM10',
-            'displayText': "PM10",
-            'checked': false
-        },
-    ]
+	let menuItems = [
+		{
+			name: 'Noise',
+			displayText: 'Noise',
+			checked: false
+		},
+		{
+			name: 'PM2.5',
+			displayText: 'PM2.5',
+			checked: false
+		},
+		{
+			name: 'PM10',
+			displayText: 'PM10',
+			checked: false
+		}
+	];
 
-    let showMenu = $state(true);
-    let content;
+	let showMenu = $state(true);
+	let content;
 
-    function handleClick(event: Event) {
-        showMenu = !showMenu;
-    }
-    function handleToggle(item: any) {
-    if (item.checked) {
-        addLayer(item.name);
-    } else {
-        removeLayer(item.name);
-    }
-}
+	function handleClick(event: Event) {
+		showMenu = !showMenu;
+	}
+	function handleToggle(item: any) {
+		if (item.checked) {
+			addLayer(item.name);
+		} else {
+			removeLayer(item.name);
+		}
+	}
 </script>
 
 <div class="menu-wrapper">
-    <button class="content" onclick={handleClick}>
-        menu
-    </button>
+	<button class="content" onclick={handleClick}> menu </button>
 
-    <div class="navbar {showMenu ? 'open' : ''}">
-        <ul>
-            {#each menuItems as item}
-                <li>
-                    <label>
-                        {item.displayText}
-                        <input 
-                            type="checkbox" 
-                            bind:checked={item.checked} 
-                            onchange={() => handleToggle(item)}
-                        />
-                    </label>
-                </li>
-            {/each}
-        </ul>
-    </div>
+	<div class="navbar {showMenu ? 'open' : ''}">
+		<ul>
+			{#each menuItems as item}
+				<li>
+					<label>
+						{item.displayText}
+						<input
+							type="checkbox"
+							bind:checked={item.checked}
+							onchange={() => handleToggle(item)}
+						/>
+					</label>
+				</li>
+			{/each}
+		</ul>
+	</div>
 </div>
 
 <style>
+	button.content {
+		all: unset;
 
-button.content {
-    all: unset;
+		font-weight: 500;
+		cursor: pointer;
 
-    font-weight: 500;
-    cursor: pointer;
+		transition: all 0.25s ease;
+	}
 
-    transition: all 0.25s ease;
-}
+	.navbar {
+		background: rgba(255, 255, 255, 0.15);
+		backdrop-filter: blur(18px);
 
-.navbar {
-    background: rgba(255, 255, 255, 0.15);
-    backdrop-filter: blur(18px);
+		overflow: hidden;
 
-    overflow: hidden;
+		/* collapsed state */
+		max-height: 0;
+		opacity: 0;
+		transform: scale(0.95);
 
-    /* collapsed state */
-    max-height: 0;
-    opacity: 0;
-    transform: scale(0.95);
+		transition:
+			max-height 0.35s ease,
+			opacity 0.25s ease,
+			transform 0.25s ease;
+	}
 
-    transition:
-        max-height 0.35s ease,
-        opacity 0.25s ease,
-        transform 0.25s ease;
-}
+	.navbar.open {
+		max-height: 300px;
+		opacity: 1;
+		transform: scale(1);
+	}
 
-.navbar.open {
-    max-height: 300px;
-    opacity: 1;
-    transform: scale(1);
-}
+	.navbar ul {
+		list-style: none;
+		margin: 0;
+	}
 
-.navbar ul {
-    list-style: none;
-    margin: 0;
-}
+	.navbar label {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
 
-.navbar label {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+		padding: 7px 5px;
+		cursor: pointer;
 
-    padding: 7px 5px;
-    cursor: pointer;
+		font-size: 14px;
+		font-weight: 500;
+		color: rgba(0, 0, 0, 0.9);
+	}
 
-    font-size: 14px;
-    font-weight: 500;
-    color: rgba(0, 0, 0, 0.9);
-}
+	.navbar input[type='checkbox'] {
+		appearance: none;
+		width: 18px;
+		height: 18px;
 
-.navbar input[type="checkbox"] {
-    appearance: none;
-    width: 18px;
-    height: 18px;
+		border-radius: 6px;
+		border: 1px solid rgba(0, 0, 0, 0.6);
 
-    border-radius: 6px;
-    border:1px solid rgba(0, 0, 0, 0.6);
+		position: relative;
+		cursor: pointer;
+		margin-left: 2px;
 
-    position: relative;
-    cursor: pointer;
-    margin-left:2px;
+		transition: all 0.2s ease;
+	}
 
-    transition: all 0.2s ease;
-}
+	.navbar input[type='checkbox']:checked {
+		background: rgba(82, 88, 94, 0.9);
+		border: none;
+	}
 
-.navbar input[type="checkbox"]:checked {
-    background: rgba(82, 88, 94, 0.9);
-    border: none;
-}
+	.navbar input[type='checkbox']:checked::after {
+		content: '';
+		position: absolute;
+		top: 3px;
+		left: 6px;
 
-.navbar input[type="checkbox"]:checked::after {
-    content: '';
-    position: absolute;
-    top: 3px;
-    left: 6px;
+		width: 4px;
+		height: 8px;
 
-    width: 4px;
-    height: 8px;
-
-    border: solid white;
-    border-width: 0 2px 2px 0;
-    transform: rotate(45deg);
-}
+		border: solid white;
+		border-width: 0 2px 2px 0;
+		transform: rotate(45deg);
+	}
 </style>
