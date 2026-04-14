83126f8afafe9d88538c5b23c7dab250cd688990
TimSchonning <116971347+TimSchonning@users.noreply.github.com>
2026-04-14 12:39:58 +0200

    feat(main.cpp): globals defined

--- node/src/main.cpp
+++ node/src/main.cpp
@@ -8,24 +8,27 @@
 // Config
 // ps = particle sensor
 // ns = noise sensor
-const int NS_PIN =       A0;
-const int CPU_FREQ_MHZ = 10;     // Not sure if this will be a const
+const uint8_t NS_PIN =       0;
+const int CPU_FREQ_MHZ =     10;     // Not sure if this will be a const
 
-const uint16_t PS_SAMPLE_TIME =    0;
-const uint16_t PS_TARGET_COUNT =   0;
+const uint32_t uS_TO_S =          1000000;
+const uint16_t PS_SAMPLE_TIME_S = 0;
+const uint16_t PS_TARGET_COUNT =  0;
+const uint32_t BAUD =             115200;
+
 const uint16_t NS_SAMPLE_TIME =    0;
 
 HM330X particle_sensor;
 uint8_t ps_sensor_buf[30];     // particle sensor buffer
 
-ps_state_t ps_state;
+ps_state_t  ps_state;
 ps_result_t ps_results;
 
-ns_state_t ns_state;
+ns_state_t  ns_state;
 ns_result_t ns_results;
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(BAUD);
 
     setCpuFrequencyMhz(CPU_FREQ_MHZ);
     WiFi.mode(WIFI_OFF);
