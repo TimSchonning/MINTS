65643975de0247f605083f0e3e3719b2af0f9163
jeni1263 <145130238+jeni1263@users.noreply.github.com>
2026-04-15 13:34:08 +0200

    show_heatmap

--- frontend/src/myapp/src/lib/Map.svelte
+++ frontend/src/myapp/src/lib/Map.svelte
@@ -1,47 +1,48 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import mapboxgl from 'mapbox-gl';
-	import 'mapbox-gl/dist/mapbox-gl.css';
-	import { toGeoJSON, testData } from '../heatmap';
+  import { onMount } from 'svelte';
+  import mapboxgl from 'mapbox-gl';
+  import 'mapbox-gl/dist/mapbox-gl.css';
+  import { toGeoJSON, testData, Data } from '../heatmap.ts';
 
-	let map: mapboxgl.Map;
+  let map : mapboxgl.Map;
 
-	onMount(() => {
-		mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+  onMount(() => {
+      mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
-		map = new mapboxgl.Map({
-			container: 'map',
-			style: 'mapbox://styles/mapbox/standard',
-			center: [17.635488, 59.843905],
-			zoom: 12
-		});
+      map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/standard',
+          center: [17.635488, 59.843905],
+          zoom: 12
+      });
 
-		map.addControl(new mapboxgl.NavigationControl());
+      map.addControl(new mapboxgl.NavigationControl());
+      
+      map.on('load', () => {
+          map.addSource('heat', {
+            type: 'geojson',
+            data: toGeoJSON([])
+          });
 
-		map.on('load', () => {
-			map.addSource('heat', {
-				type: 'geojson',
-				data: toGeoJSON([])
-			});
+          map.addLayer({
+              id: 'heatmap',
+              type: 'heatmap',
+              source: 'heat',
+              maxzoom: 20,
+              paint: {
+                'heatmap-weight': ['get', 'intensity'],
+                'heatmap-radius': 20
+              }
+              });
+          Data.subscribe(data => {
+            const source = map.getSource('heat') as mapboxgl.GeoJSONSource;
+            if (source) {
+              source.setData(toGeoJSON(data));
+            }
+          })
+  });
+});
 
-			map.addLayer({
-				id: 'heatmap',
-				type: 'heatmap',
-				source: 'heat',
-				maxzoom: 20,
-				paint: {
-					'heatmap-weight': ['get', 'intensity'],
-					'heatmap-radius': 20
-				}
-			});
-			testData.subscribe((data) => {
-				const source = map.getSource('heat') as mapboxgl.GeoJSONSource;
-				if (source) {
-					source.setData(toGeoJSON(data));
-				}
-			});
-		});
-	});
 </script>
 
 <div id="map" style="width: 100vw; height: 100vh;"></div>
