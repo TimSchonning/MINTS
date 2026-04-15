20c833b5e251d87d86c30f42de7131b58fa84669
TimSchonning <116971347+TimSchonning@users.noreply.github.com>
2026-04-15 16:59:53 +0200

    docs: added documentation to utils.cpp
    Functions affected:
    error_handler
    power_down_radios
    initialise_node

--- node/include/utils.h
+++ node/include/utils.h
@@ -3,10 +3,24 @@
 
 #include <cstdint>
 
+/**
+ * @brief Logic for handling radio status codes.
+ * @param state The return code from the radio library.
+ * @param message The error message to print if the state is not successful.
+ * @return true if an error occurred, false if successful.
+ */
 bool error_handler(int16_t state, const char* message);
 
+/**
+ * @brief Disables all wireless communication.
+ */
 void power_down_radios();
 
+/**
+ * @brief Initializes the LoRa radio and performs a handshake with the gateway
+ * to receive a unique node ID.
+ * @note Sets init_flag to false upon success.
+ */
 void initialise_node();
 
 #endif
