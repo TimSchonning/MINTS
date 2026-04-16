e238a729c7397e6114997dc75c3b3ad1919f775f
Filip Shellbranch <filip.hellgren04@gmail.com>
2026-04-16 14:01:57 +0200

    feat: huuuuh

--- frontend/src/myapp/src/lib/ChangeDate.svelte
+++ frontend/src/myapp/src/lib/ChangeDate.svelte
@@ -1,24 +1,23 @@
 <script lang="ts">
-	import ChevronLeftIcon from '@lucide/svelte/icons/chevron-Left';
+	import ChevronLeftIcon from '@lucide/svelte/icons/chevron-left';
 	import ChevronRightIcon from '@lucide/svelte/icons/chevron-right';
 	import { shown_date } from '../map_controller';
 
-    function handleClickLeft() {
-        shown_date.setDate(shown_date.getDate() - 1);
-    }
-    function handleClickRight() {
-        if (shown_date.getDate() === new Date().getDate()) {
-            return;
-        }
-        else {
-            shown_date.setDate(shown_date.getDate() + 1);
-        }
-    }
-
+	function handleClickLeft() {
+		shown_date.setDate(shown_date.getDate() - 1);
+	}
+	function handleClickRight() {
+		if (shown_date.getDate() === new Date().getDate()) {
+			return;
+		} else {
+			shown_date.setDate(shown_date.getDate() + 1);
+		}
+	}
 </script>
 
 <div class="inline-flex -space-x-px rounded-full shadow-2xs rtl:space-x-reverse">
-	<button onclick={handleClickLeft}
+	<button
+		onclick={handleClickLeft}
 		class="rounded-none shadow-none first:rounded-s-full last:rounded-e-full focus-visible:z-10"
 		aria-label="Upvote"
 	>
@@ -26,8 +25,9 @@
 	</button>
 	<button class="bg-primary text-primary-foreground flex items-center px-1 text-sm font-medium">
 		{shown_date.toLocaleDateString('en-GB', { month: 'long', day: 'numeric', year: 'numeric' })}
-    </button>
-	<button onclick={handleClickRight}
+	</button>
+	<button
+		onclick={handleClickRight}
 		class="rounded-none shadow-none first:rounded-s-full last:rounded-e-full focus-visible:z-10"
 		aria-label="Downvote"
 	>
@@ -36,5 +36,4 @@
 </div>
 
 <style>
-    
 </style>
