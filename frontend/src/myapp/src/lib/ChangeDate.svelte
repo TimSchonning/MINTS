53dc20672f533e444a6bb0fd8c1c4da0c302537f
jeni1263 <145130238+jeni1263@users.noreply.github.com>
2026-04-16 12:51:37 +0200

    dateknapp

---
+++ frontend/src/myapp/src/lib/ChangeDate.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import ChevronLeftIcon from '@lucide/svelte/icons/chevron-Left';
+	import ChevronRightIcon from '@lucide/svelte/icons/chevron-right';
+	import { shown_date } from '../map_controller';
+
+    function handleClickLeft() {
+        shown_date.setDate(shown_date.getDate() - 1);
+    }
+    function handleClickRight() {
+        if (shown_date.getDate() === new Date().getDate()) {
+            return;
+        }
+        else {
+            shown_date.setDate(shown_date.getDate() + 1);
+        }
+    }
+
+</script>
+
+<div class="inline-flex -space-x-px rounded-full shadow-2xs rtl:space-x-reverse">
+	<button onclick={handleClickLeft}
+		class="rounded-none shadow-none first:rounded-s-full last:rounded-e-full focus-visible:z-10"
+		aria-label="Upvote"
+	>
+		<ChevronLeftIcon size={16} aria-hidden="true" />
+	</button>
+	<button class="bg-primary text-primary-foreground flex items-center px-1 text-sm font-medium">
+		{shown_date.toLocaleDateString('en-GB', { month: 'long', day: 'numeric', year: 'numeric' })}
+    </button>
+	<button onclick={handleClickRight}
+		class="rounded-none shadow-none first:rounded-s-full last:rounded-e-full focus-visible:z-10"
+		aria-label="Downvote"
+	>
+		<ChevronRightIcon size={16} aria-hidden="true" />
+	</button>
+</div>
+
+<style>
+    
+</style>
