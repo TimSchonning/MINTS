585135e2e5318cd247fc8cef659ead727c8ea08f
DaOl6717 <davidolmedo011@gmail.com>
2026-05-04 15:53:22 +0200

    Edited settings

--- gateway/src/gatewayLogic.cpp
+++ gateway/src/gatewayLogic.cpp
@@ -25,10 +25,10 @@ int CS = 21, DIO1 = 16, BUSY = 18, RST = 20;
 
 float FREQ = 868.1; // Frequency
 float BW = 125.0;   // Bandwidth
-int SF = 7;         // Spreading Factor
+int SF = 8;         // Spreading Factor
 int CR = 8;         // Coding Rate
 int SYNC = 0x12;    // Sync word
-int PWR = 10;       // Power
+int PWR = 13;       // Power
 int PRE = 8;        // Preamble
 int BAUD = 115200;  // Baud
 
