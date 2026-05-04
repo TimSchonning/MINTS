54cd71362a3bb9d594e407cfa77526acaf5f8389
DaOl6717 <davidolmedo011@gmail.com>
2026-05-04 12:53:19 +0200

    Small fixes

--- gateway/src/gatewayLogic.cpp
+++ gateway/src/gatewayLogic.cpp
@@ -50,10 +50,10 @@ bool IdAssignment() {
     // TODO: db error handling
     
     msg_ack_t idReqACK;
-    idReqACK.id = id;  // The new ID!
+    idReqACK.node_id = id;  // The new ID!
     idReqACK.ack_for = MSG_TYPE_JOIN_REQ;
 
-    int state = radio.transmit(idReqACK, sizeof(msg_ack_t));
+    int state = radio.transmit((uint8_t*)&idReqACK, sizeof(msg_ack_t));
     //error_handler(state);  // TODO: error handling
     return (state == RADIOLIB_ERR_NONE); // If succeeded return true
 }
