ce3d6e3dbd54b5215c77dcb6db9cfebb7835034e
TimSchonning <116971347+TimSchonning@users.noreply.github.com>
2026-05-05 09:25:53 +0200

    fix(macros): macros adapted towards cpp

--- gateway/include/debug_macros.h
+++ gateway/include/debug_macros.h
@@ -1,16 +1,16 @@
 #ifndef DEBUG_MACROS_H
 #define DEBUG_MACROS_H
 
+#include <iostream>
+
 #define DEBUG_MODE // Comment this to disable debug mode
 
 #ifdef DEBUG_MODE
-  #define DEBUG_PRINT(x) Serial.print(x)
-  #define DEBUG_PRINTLN(x) Serial.println(x)
-  #define DEBUG_BEGIN(x) Serial.begin(x)
+  #define DEBUG_PRINT(x)   std::cout << (x)
+  #define DEBUG_PRINTLN(x) std::cout << (x) << std::endl
 #else
   #define DEBUG_PRINT(x)
   #define DEBUG_PRINTLN(x)
-  #define DEBUG_BEGIN(x)
 #endif
 
 #endif
