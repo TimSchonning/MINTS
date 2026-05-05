e3f2bb5c22a03431ce90e1839c37af1f34bb9a00
TimSchonning <116971347+TimSchonning@users.noreply.github.com>
2026-05-05 09:41:53 +0200

    feat(utils): error handler added

---
+++ gateway/src/utils.cpp
@@ -0,0 +1,20 @@
+#include <cstdint>
+
+#include "../include/debug_macros.h"
+#include "RadioLib.h"
+
+bool error_handler(int16_t state, const char* message) {
+    if (state != RADIOLIB_ERR_NONE) {
+        DEBUG_PRINT("[ERROR] ");
+        DEBUG_PRINT(message);
+        DEBUG_PRINT(" Code: ");
+        DEBUG_PRINTLN(state);
+        
+        #ifdef DEBUG_MODE
+            while (1); 
+        #endif
+        return true;
+    } else {
+        return false;
+    }
+}
