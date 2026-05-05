a7bc5afb536a296341b6d97b126ff94432d0c497
TimSchonning <116971347+TimSchonning@users.noreply.github.com>
2026-05-05 09:27:05 +0200

    fix: id_assignment omitted
    Deprecated.

--- gateway/src/gatewayLogic.cpp
+++ gateway/src/gatewayLogic.cpp
@@ -40,18 +40,18 @@ void LoRaInit() {
     }
 }
 
-bool IdAssignment() {
-    uint8_t id = 1;     //TODO: ID from db. 1 is placeholder.
-    // TODO: db error handling
+// bool IdAssignment() {
+//     uint8_t id = 1;     //TODO: ID from db. 1 is placeholder.
+//     // TODO: db error handling
     
-    msg_ack_t idReqACK;
-    idReqACK.node_id = id;  // The new ID!
-    idReqACK.ack_for = MSG_TYPE_JOIN_REQ;
+//     msg_ack_t idReqACK;
+//     idReqACK.node_id = id;  // The new ID!
+//     idReqACK.ack_for = MSG_TYPE_JOIN_REQ;
 
-    int state = radio.transmit((uint8_t*)&idReqACK, sizeof(msg_ack_t));
-    //error_handler(state);  // TODO: error handling
-    return (state == RADIOLIB_ERR_NONE); // If succeeded return true
-}
+//     int state = radio.transmit((uint8_t*)&idReqACK, sizeof(msg_ack_t));
+//     //error_handler(state);  // TODO: error handling
+//     return (state == RADIOLIB_ERR_NONE); // If succeeded return true
+// }
 
 int main() // TODO: Clear gateway simulation and add (modified) main loop from LoRa.cpp
 {
