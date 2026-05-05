8826f3e2bf4849fb87a45b4f7743e74d7dcec64f
TimSchonning <116971347+TimSchonning@users.noreply.github.com>
2026-05-05 09:20:24 +0200

    feat: protocol updated
    msg_config omitted
    msg_tag omitted

--- gateway/include/protocol.h
+++ gateway/include/protocol.h
@@ -1,12 +1,30 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
 
-// Types of messages
-const uint8_t MSG_TYPE_ACK              = 0xA;
-const uint8_t MSG_TYPE_PAYLOAD_UPLINK   = 0xB;  // node -> gateway
+#include <stdint.h>
 
-const uint8_t MSG_TYPE_JOIN_REQ         = 0xD;
-const uint8_t MSG_TYPE_START_DOWNLINK   = 0xF;
+// Types of messages
+const uint8_t MSG_TYPE_ACK            = 0xA0;
+const uint8_t MSG_TYPE_PAYLOAD_UPLINK = 0xB0;  // node -> gateway
+
+const uint8_t MSG_TYPE_JOIN_REQ       = 0xD0;
+const uint8_t MSG_TYPE_CLEARANCE      = 0xD1;
+
+const uint8_t MSG_TYPE_CONFIG         = 0xF0;
+
+/**
+ * @brief Stores the results as a sendable LoRa payload.
+ * @note reading_id reflects the latest readings id.
+ * @note Is __attribute__((packed)
+ */
+typedef struct __attribute__((packed)) {
+    uint8_t type;
+    uint8_t node_id;
+    uint8_t reading_id;     // locally unique
+    uint8_t readings[BUFFERING_THRESHOLD * 4]
+    // uint8_t pm10, pm25;
+    // uint16_t noise_peak;
+} payload_t;
 
 /**
  * @brief Ack message structure.
@@ -15,7 +33,7 @@ const uint8_t MSG_TYPE_START_DOWNLINK   = 0xF;
 typedef struct __attribute__((packed)) {
     uint8_t type = MSG_TYPE_ACK;
     uint8_t node_id;
-    uint8_t ack_for;    //i.e which type of msg is being ack:ed
+    uint8_t ack_for;    //ie which type of msg is being ack:ed
 } msg_ack_t;
 
 /**
@@ -26,5 +44,16 @@ typedef struct __attribute__((packed)) {
     uint8_t type;
     uint8_t node_id;
 } msg_req_t;
+
+/**
+ * @brief Structure for gateway synchronization clearance
+ * @note Will ever only be received from the gateway upon initialisation.
+ * @note Is __attribute__((packed)
+ */
+typedef struct __attribute__((packed)) {
+    uint8_t type;
+    uint32_t time_stamp;
+} msg_clearance_t;
+
 
 #endif
