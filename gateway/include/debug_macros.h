8846f89b49c595bf3db08476edaa841226362fb2
TimSchonning <116971347+TimSchonning@users.noreply.github.com>
2026-05-05 09:29:39 +0200

    fix: minor macro bug fix

--- gateway/include/debug_macros.h
+++ gateway/include/debug_macros.h
@@ -6,7 +6,7 @@
 #define DEBUG_MODE // Comment this to disable debug mode
 
 #ifdef DEBUG_MODE
-  #define DEBUG_PRINT(x)   std::cout << (x)
+  #define DEBUG_PRINT(x)   (std::cout << (x)).flush()
   #define DEBUG_PRINTLN(x) std::cout << (x) << std::endl
 #else
   #define DEBUG_PRINT(x)
