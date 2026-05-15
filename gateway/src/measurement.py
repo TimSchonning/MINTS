613a8ec5604c21f2998ddd606d83dbbb5a54ff75
DaOl6717 <davidolmedo011@gmail.com>
2026-05-15 09:44:08 +0200

    Fix: Added the correct timezone.

--- gateway/src/measurement.py
+++ gateway/src/measurement.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 from datetime import timedelta
+import zoneinfo
 
 NODE_SAMPLE_WINDOW_S = 900
+
+time_zone = zoneinfo.ZoneInfo("Europe/Stockholm")
 
 class MeasurementGroup():
     readings = {}
@@ -10,5 +13,5 @@ class MeasurementGroup():
         self.readings["PM2.5"] = pm25
         self.readings["PM1"] = pm1
         self.readings["Noise"] = noise
-        self.timestamp = (datetime.now() - timedelta(seconds=(int(batch_nr) * NODE_SAMPLE_WINDOW_S))).replace(second=0, microsecond=0)
+        self.timestamp = (datetime.now(time_zone) - timedelta(seconds=(int(batch_nr) * NODE_SAMPLE_WINDOW_S))).replace(second=0, microsecond=0)
         
